8aee3c438bf923f61e2ca05758cf58a073557551
Blythes <145576625+soudayonee@users.noreply.github.com>
2025-12-17 12:43:20 +0700

    Change audio source to local bad-result.mp3

--- script.js
+++ script.js
@@ -114,9 +114,7 @@ function showBadResult(result) {
   headerText.style.display = "none";
   inputContainer.style.display = "none";
   resultContainer.style.display = "flex";
-  audio.src =
-    "https://rr1---sn-npoe7nlz.googlevideo.com/videoplayback?expire=1765901539&ei=gzBBadeOIdSX4t4P-92ocQ&ip=112.140.167.62&id=o-AP3SLaWwO8rgytaKZDuU8c-qcbiLcFCJ7GoWljlr-IbU&itag=251&source=youtube&requiressl=yes&xpc=EgVo2aDSNQ%3D%3D&cps=129&gcr=id&bui=AYUSA3BSp5_vmzYxIBM19NgH7qgkNf3WOiJ_PJwYGcI4Cm8K05W1LRw30SBAj2GmMv055wEM3z4e_vtI&vprv=1&svpuc=1&mime=audio%2Fwebm&ns=LoM-Dks5R2_r5zQRtCrZrUER&rqh=1&gir=yes&clen=4168292&dur=252.101&lmt=1726309572217716&keepalive=yes&lmw=1&fexp=51557447,51565115,51565681,51580970&c=TVHTML5&sefc=1&txp=5532434&n=dJq5aPINmtrOLA&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cxpc%2Cgcr%2Cbui%2Cvprv%2Csvpuc%2Cmime%2Cns%2Crqh%2Cgir%2Cclen%2Cdur%2Clmt&sig=AJfQdSswRAIgN-_dnn6OW4Q8QfcNrLW0eA7J5eM-J4oyhg7NibjwQ2wCIDl4Nn7A9vNeEdcYH66tkY1QXarPfZrihqZzZbJzI2n3&rm=sn-2uuxa3vh-n0cs7l,sn-npozs7e&rrc=79,104&req_id=8a6f9b6720b7a3ee&rms=rdu,au&redirect_counter=2&cms_redirect=yes&cmsv=e&ipbypass=yes&met=1765879956,&mh=OU&mip=103.224.124.165&mm=29&mn=sn-npoe7nlz&ms=rdu&mt=1765879740&mv=m&mvi=1&pl=24&lsparams=cps,ipbypass,met,mh,mip,mm,mn,ms,mv,mvi,pl,rms&lsig=APaTxxMwRQIhALx4RJGfjSYqzm2qNlkJnuzszjmrUHRraHcgVk3rwmrRAiBgMK5Jzy_ROlQJ1TIED653pRA7z8C4cQ8-9yq3i-4ODA%3D%3D";
-
+  audio.src = "./bad-result.mp3";
   document.body.style.backgroundColor = "rgba(0, 0, 0, 1)";
 
   setTimeout(() => {
